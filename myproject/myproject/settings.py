3978aed39dd52eaa77b8a79dd62d22759f02bb35
tanuj1221 <75058861+tanuj1221@users.noreply.github.com>
2024-03-29 23:44:14 +0530

    22
    12

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-0l+fpd&z*tf!%bb7=nigpo^af-g0dd2sea$+lbg4+&ek9)ml1e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.201.37.88', '13.201.37.88:8000']
 
 # settings.py
 
